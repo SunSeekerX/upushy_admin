9a9966653a128f45c0fb19e0d9a9ec014af29d82
SunSeekerX <1647800606@qq.com>
2021-07-12 01:12:49 +0800

    build: vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
+/**
+ * @name:
+ * @author: SunSeekerX
+ * @Date: 2021-04-26 00:46:14
+ * @LastEditors: SunSeekerX
+ * @LastEditTime: 2021-07-11 12:34:10
+ */
 const path = require('path')
 const webpack = require('webpack')
 const createThemeColorReplacerPlugin = require('./config/plugin.config')
@@ -6,27 +13,28 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
-const assetsCDN = {
-  // webpack build externals
-  externals: {
-    vue: 'Vue',
-    'vue-router': 'VueRouter',
-    vuex: 'Vuex',
-    axios: 'axios',
-  },
-  css: [],
-  js: [
-    '//cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.min.js',
-    '//cdn.jsdelivr.net/npm/vue-router@3.4.9/dist/vue-router.min.js',
-    '//cdn.jsdelivr.net/npm/vuex@3.5.1/dist/vuex.min.js',
-    '//cdn.jsdelivr.net/npm/axios@0.21.0/dist/axios.min.js',
-  ],
-}
+// const assetsCDN = {
+//   // webpack build externals
+//   externals: {
+//     vue: 'Vue',
+//     'vue-router': 'VueRouter',
+//     vuex: 'Vuex',
+//     axios: 'axios',
+//   },
+//   css: [],
+//   js: [
+//     '//cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.min.js',
+//     '//cdn.jsdelivr.net/npm/vue-router@3.4.9/dist/vue-router.min.js',
+//     '//cdn.jsdelivr.net/npm/vuex@3.5.1/dist/vuex.min.js',
+//     '//cdn.jsdelivr.net/npm/axios@0.21.0/dist/axios.min.js',
+//   ],
+// }
 
 // vue.config.js
 const vueConfig = {
+  publicPath: './',
   configureWebpack: {
     // webpack plugins
     plugins: [
@@ -39,7 +47,7 @@ const vueConfig = {
       // }),
     ],
     // if prod, add externals
-    externals: isProd ? assetsCDN.externals : {},
+    // externals: isProd ? assetsCDN.externals : {},
   },
 
   chainWebpack: config => {
@@ -63,12 +71,12 @@ const vueConfig = {
 
     // if prod is on
     // assets require on cdn
-    if (isProd) {
-      config.plugin('html').tap(args => {
-        args[0].cdn = assetsCDN
-        return args
-      })
-    }
+    // if (isProd) {
+    //   config.plugin('html').tap(args => {
+    //     args[0].cdn = assetsCDN
+    //     return args
+    //   })
+    // }
   },
 
   css: {
